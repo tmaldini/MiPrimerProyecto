4dc6fc972fe6d13e99a2ba9018c9e5ca0e12d321
tamaldini <tamaldini@cnv.gov.ar>
2026-09-22 12:45:24 -0300

    Ejercicio 3

--- app/src/main/java/com/example/miprimerproyecto/MainActivity.kt
+++ app/src/main/java/com/example/miprimerproyecto/MainActivity.kt
@@ -5,12 +5,14 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.modifier.modifierLocalProvider
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.miprimerproyecto.ui.theme.MiPrimerProyectoTheme
@@ -22,7 +24,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             MiPrimerProyectoTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Presentacion(modifier = Modifier.padding(innerPadding))
+                    FichaDelEstudiante(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -30,20 +32,26 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Presentacion(modifier: Modifier = Modifier) {
-    Column() {
-        Text(
-            text = "nombre",
-            modifier = modifier.padding(all = 10.dp)
-        )
-        Text(
-            text = "carrera",
-            modifier = modifier.padding(all = 10.dp)
-        )
-        Text(
-            text = "una tecnologia que el estudiante quiera aprender",
-            modifier = modifier.padding(all = 10.dp)
-        )
+fun FichaDelEstudiante(modifier: Modifier = Modifier) {
+    val nombre = "Ana"
+    val edad = 20
+    val promedio = 8.25
+    val cursaProgramacion = true
+    val anioProximo= edad+1
+    val documento: String= "24546456"
+    val materias: Int = 5
+    val ciudad: String= "Cordoba"
+
+    Column(modifier) {
+        Text("Nombre $nombre", modifier = Modifier.padding(all = 30.dp) )
+        Text("Edad $edad", modifier = Modifier.padding(all = 30.dp))
+        Text("Promedio $promedio", modifier = Modifier.padding(all = 30.dp))
+        Text("CursaProgramacion $cursaProgramacion", modifier = Modifier.padding(all = 30.dp))
+        Text("AñoProximo $anioProximo", modifier = Modifier.padding(all = 30.dp))
+        Text("Documento: $documento" , modifier = Modifier.padding(all = 30.dp))
+//        Otra forma de mostrar los textos
+        Text("Materias:" + materias, modifier = Modifier.padding(all = 30.dp))
+        Text("Ciudad:" + ciudad, modifier = Modifier.padding(all = 30.dp))
     }
 }
 
@@ -51,6 +59,6 @@ fun Presentacion(modifier: Modifier = Modifier) {
 @Composable
 fun PresentacionPreview() {
     MiPrimerProyectoTheme {
-        Presentacion()
+        FichaDelEstudiante()
     }
 }
