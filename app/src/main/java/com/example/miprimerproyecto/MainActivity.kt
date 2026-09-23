d83c844cf262042f86316db802604c6aeed4974f
TOMAS MALDINI <tmaldini@uade.edu.ar>
2026-09-23 20:25:02 -0300

    commit del ejercicio 5

--- app/src/main/java/com/example/miprimerproyecto/MainActivity.kt
+++ app/src/main/java/com/example/miprimerproyecto/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,6 +14,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.miprimerproyecto.ui.theme.MiPrimerProyectoTheme
@@ -24,9 +26,15 @@ class MainActivity : ComponentActivity() {
         setContent {
             MiPrimerProyectoTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Column(modifier = Modifier.padding(innerPadding).padding(16.dp)) {
+                    Column(modifier = Modifier
+                        .padding(innerPadding)
+                        .padding(16.dp)
+                        .background(Color.Red)
+
+                    ) {
+                        Text(text = "Ficha del estudiante")
                         DatoDelEstudiante(etiqueta = "Nombre", valor = "Juan")
-                        DatoDelEstudiante(etiqueta = "Edad", valor = DescripcionEdad(20))
+                        DatoDelEstudiante(etiqueta = "Edad", valor = descripcionEdad(20))
                         DatoDelEstudiante(etiqueta = "Carrera", valor = "Sistemas" )
                         DatoDelEstudiante(etiqueta = "Anio", valor = "1" )
                         Botoncito()
@@ -45,8 +53,7 @@ fun DatoDelEstudiante(etiqueta: String, valor: String, modifier: Modifier = Modi
     }
 }
 
-@Composable
-fun DescripcionEdad(edad: Int): String {
+fun descripcionEdad(edad: Int): String {
     if (edad >= 18) {
         return "Es mayor de edad, edad: $edad años"
     }
@@ -66,9 +73,13 @@ fun Botoncito() {
 @Composable
 fun PresentacionPreview() {
     MiPrimerProyectoTheme {
-        Column(modifier = Modifier.padding(16.dp)) {
+        Column(modifier = Modifier
+            .padding(16.dp)
+            .background(Color.Red)
+
+        ) {
             DatoDelEstudiante(etiqueta = "Nombre", valor = "Juan")
-            DatoDelEstudiante(etiqueta = "Edad", valor = DescripcionEdad(20))
+            DatoDelEstudiante(etiqueta = "Edad", valor = descripcionEdad(20))
             Botoncito()
         }
     }
